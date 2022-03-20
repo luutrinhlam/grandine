9d948808d76fc18d66c0cef548b85c1aa490a507
Lam <luutrinhlam@gmail.com>
2022-03-21 02:22:25 +0700

    Publish Email to MQTT

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import React from 'react';
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
+import { Connector } from 'mqtt-reactjs-hooks';
+
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <div className="gradient__bg">
-      <Navbar />
-      <Header />
-      <Brand />
+const options = {
+  // servers:[{host: 'localhost', port: 1883}], //optional servers settings
+  keepAlive: 60, // in seconds
+  clientId: 'mqttjs_react',
+  protocolId: 'MQTT',
+  protocolVersion: 4,
+  clean: true, // set to false to receive QoS 1 and 2 messages while offline
+  reconnectPeriod: 1000, // milliseconds, interval between two reconnections. Disable auto reconnect by setting to 0.
+  username: 'luutrinhlam', //the username required by your broker, if any
+  password: 'aio_IZlb68f98u51WmafiI40YDBKvI5F' //the password required by your broker, if any
+}
 
+const App = () => (
+  <Connector brokerUrl='https://io.adafruit.com' options={options}>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+        <Brand />
+
+      </div>
+      <div className="color__bg">
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
     </div>
-    <div className="color__bg">
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
-    </div>
-  </div>
+  </Connector>
 );
 
 export default App;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
-import people from '../../assets/people.png';
+// import people from '../../assets/people.png';
 import ai from '../../assets/dragon.png';
 import './header.css';
+import { useState } from 'react'
+import { useMqttState, useSubscription } from 'mqtt-reactjs-hooks';
 
-const Header = () => (
-  <div className="gpt3__header section__padding" id="home">
-    <div className="gpt3__header-content">
-      <h1 className="gradient__text">With the power of Grandine, weather is no matter!</h1>
-      <p>Using AI, mordern Grandine can predict tomorrow sky, and collect all the weather statistics.<br></br> Everything just for you</p>
 
-      <div className="gpt3__header-content__input">
-        <input type="email" placeholder="Your Email Address" />
-        <button type="button">Get Notification</button>
-      </div>
 
-      {/* <div className="gpt3__header-content__people">
+
+function Header() {
+  const { mqttClient } = useMqttState();
+
+  const [ email, setEmail ] = useState('');
+
+
+
+  function SubmitEmail(email) {
+    alert('Thank for your subcription! Grandine will be there with your weather');
+    return mqttClient.publish('luutrinhlam/feeds/bbc-temp', email);  
+  }
+
+  function handleInputChange(event){
+    const value = event.target.value;
+    setEmail(value);
+  }
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">With the power of Grandine, weather is no matter!</h1>
+        <p>Using AI, mordern Grandine can predict tomorrow sky, and collect all the weather statistics.<br></br> Everything just for you</p>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" onChange={handleInputChange}/>
+          <button type="button" onClick={() => SubmitEmail(email)}>Get Notification</button>
+        </div>
+
+        {/* <div className="gpt3__header-content__people">
         <img src={people} />
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div> */}
-    </div>
+      </div>
 
-    <div className="gpt3__header-image">
-      <img src={ai} />
+      <div className="gpt3__header-image">
+        <img src={ai} />
+      </div>
     </div>
-  </div>
-);
+  )
+};
+
 
 export default Header;
