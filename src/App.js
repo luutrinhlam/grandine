fd49009c53de0ca625cbf4622265574b8e2208de
Lam <luutrinhlam@gmail.com>
2022-03-24 01:07:20 +0700

    modify header

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const options = {
   clean: true, // set to false to receive QoS 1 and 2 messages while offline
   reconnectPeriod: 1000, // milliseconds, interval between two reconnections. Disable auto reconnect by setting to 0.
   username: 'luutrinhlam', //the username required by your broker, if any
-  password: 'aio_IZlb68f98u51WmafiI40YDBKvI5F' //the password required by your broker, if any
+  password: 'aio_Acmc74HTI9tJ3gZXjhJkMfjvAeJP' //the password required by your broker, if any
 }
 
 const App = () => (
